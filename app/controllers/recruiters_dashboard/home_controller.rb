e81edeb849bbc70eb8252f9b32087d274aede6a0
Cleyton Silva <silva.rcleyton@gmail.com>
2023-05-05 05:35:38 -0300

    list jobs on dashboard

--- app/controllers/recruiters_dashboard/home_controller.rb
+++ app/controllers/recruiters_dashboard/home_controller.rb
@@ -1,4 +1,7 @@
 class RecruitersDashboard::HomeController < RecruitersDashboardController
   def index
+    def index
+      @vacancies = Vacancy.where(recruiter_profile_id: current_recruiter)
+    end
   end
 end
