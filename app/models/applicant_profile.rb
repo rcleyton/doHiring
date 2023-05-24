a1deead0d4e07cb40111415ffbd94c16b036e061
Cleyton <silva.rcleyton@gmail.com>
2023-05-24 19:35:01 -0300

    add validations to applicant profile model

--- app/models/applicant_profile.rb
+++ app/models/applicant_profile.rb
@@ -1,4 +1,7 @@
 class ApplicantProfile < ApplicationRecord
+  validates :first_name, :last_name, :birthdate, :rg, :cpf, :gender,
+            :civil_status, :professional_goals, presence: true
+            
   belongs_to :applicant
   has_many :candidatures
   has_many :vacancies, through: :candidatures
