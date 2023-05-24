4d2a3ef81f726901326edc1d95a9e798e41c13c2
Cleyton <silva.rcleyton@gmail.com>
2023-05-24 19:22:29 -0300

    add validations to recruiter profile model

--- app/models/recruiter_profile.rb
+++ app/models/recruiter_profile.rb
@@ -1,3 +1,6 @@
 class RecruiterProfile < ApplicationRecord
+  validates :full_name, :document, :phone, :cell_phone, :sector,
+            :number_employee, presence: true
+            
   belongs_to :recruiter
 end
