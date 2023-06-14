ada88aaa966d7236c441e3bc7c2d8487a26a4d1d
Cleyton <silva.rcleyton@gmail.com>
2023-06-14 18:35:09 -0300

    add validation and I18n to proposal model

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,4 +1,5 @@
 class Proposal < ApplicationRecord
-  validates :start_date, presence: true
+  validates :start_date, :salary_offered, :benefits_offered, 
+  :job_functions, :company_expectations, :bonus, :additional_information, presence: true
   belongs_to :candidature
 end
